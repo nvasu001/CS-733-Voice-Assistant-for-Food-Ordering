f2d18ce0c517c295dcf9815ae7ee9309219dc7e7
Neeraja Vasudevan <nvasu001@odu.edu>
2022-12-03 20:04:28 -0500

    asr changes

--- asr.py
+++ asr.py
@@ -1,7 +1,7 @@
 from vosk import Model, KaldiRecognizer
 import pyaudio
 
-model = Model(r'C:\Users\vasud\Desktop\3rd_sem_hw\CS733\CS-733-Voice-Assistant-for-Food-Ordering\CS-733-Voice-Assistant-for-Food-Ordering\vosk-model-small-en-us-0.15') # Read the model
+model = Model(r'vosk-model-small-en-us-0.15') # Read the model
 recognizer = KaldiRecognizer(model, 16000)
 
 # Recognize from the microphone
